688b8089f904982b6832d94d7103e98630e8b91a
Arshia239 <arshianoonari2000@gmail.com>
2026-06-26 21:47:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  document.querySelectorAll('.tab').forEach(btn => {
+    btn.addEventListener('click', () => {
+      document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+      btn.classList.add('active');
+    });
+  });
+</script>
